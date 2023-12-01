7fc55abbfa5f9936cdeb32c2af04f9c56253311b
marquinhos2017 <marcosrodriguescorreiajc@gmail.com>
2023-12-01 11:18:30 +0000

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll',function(){
+        const positionAtual = window.scrollY
+        
+        if(positionAtual < alturaHero){
+            ocultaElementosdoHeader();
+        }else{
+            exibeElementosdoHeader();
+        }
+    })
+
+    for(let i=0; i<buttons.length;i++){
+        buttons[i].addEventListener('click', function(botao){
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
+            
+        })
+    }
+
+    for(let i = 0; i< questions.length;i++){
+        questions[i].addEventListener('click',abreouFechaResposta);
+    }
+})
+
+function exibeElementosdoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function ocultaElementosdoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function abreouFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+function removeBotaoAtivo(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for(let i=0; i<buttons.length;i++){
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas(){
+    const tabsContainer =  document.querySelectorAll('[data-tab-id');
+
+    for(let i = 0; i< tabsContainer.length;i++){
+        tabsContainer[i].classList.remove('shows__list--is-active');
+
+    }
+}
